a70cb5f184c94935d1726e74fa86873946814724
WesleyL168 <wesleylim168@gmail.com>
2022-11-07 13:10:29 -0500

    commit 01

--- test_world.py
+++ test_world.py
@@ -1,1 +1,8 @@
-print("hello world")
+weight = float(input("What is your weight? "))
+type = input("(K)g or (L)bs: ")
+if type == "K" or "k":
+    converted = weight * 2.2
+    print("Weight in lbs: " + str(converted))
+else:
+    converted = weight * 0.45
+    print("Weight in kg: " + str(converted))
