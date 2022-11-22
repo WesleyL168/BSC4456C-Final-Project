86e59a639934e437834859cf441a4d59c35ccb18
WesleyL168 <wesleylim168@gmail.com>
2022-11-22 16:45:39 -0500

    commit 11/22

---
+++ data_process.R
@@ -0,0 +1,7 @@
+### Setup
+install.packages("tidytuesdayR")
+library(tidyverse)
+pell <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-08-30/pell.csv')
+
+glimpse(pell)
+
